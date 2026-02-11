cda8638a22a6294b4b78bfe20c8aa3f7efdcf8ce
echofield <echofield@outlook.fr>
2026-02-11 13:02:09 +0100

    test: proxy in different file name

---
+++ api/card-gate/pair2.js
@@ -0,0 +1,31 @@
+/**
+ * Proxy test in new file
+ */
+
+module.exports = function handler(req, res) {
+  var https = require('https');
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(204).end();
+
+  var proxyReq = https.request({
+    hostname: 'qvyrpzgxsppkwfvqvgcn.supabase.co',
+    port: 443,
+    path: '/functions/v1/card-gate/pair',
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  }, function(proxyRes) {
+    var d = '';
+    proxyRes.on('data', function(c) { d += c; });
+    proxyRes.on('end', function() {
+      res.status(proxyRes.statusCode).send(d);
+    });
+  });
+
+  proxyReq.on('error', function(e) {
+    res.status(500).json({ err: e.message });
+  });
+
+  proxyReq.write('{}');
+  proxyReq.end();
+};
