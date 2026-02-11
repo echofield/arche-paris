6a4dac05d50c4ebfe04b7c0a1c1d5f0553214633
echofield <echofield@outlook.fr>
2026-02-11 12:46:20 +0100

    test: https.request with callback

--- api/card-gate/pair.js
+++ api/card-gate/pair.js
@@ -1,11 +1,10 @@
 /**
- * Test: does https.request cause boot error?
+ * Test: https.request with callback
  */
 
 module.exports = function handler(req, res) {
   var https = require('https');
 
-  // Just test if we can call https.request without error
   var options = {
     hostname: 'example.com',
     port: 443,
@@ -13,13 +12,22 @@ module.exports = function handler(req, res) {
     method: 'GET'
   };
 
-  // Create request but immediately abort it
-  var testReq = https.request(options);
+  var testReq = https.request(options, function(proxyRes) {
+    // Callback that never runs because we abort
+    var chunks = [];
+    proxyRes.on('data', function(chunk) {
+      chunks.push(chunk);
+    });
+    proxyRes.on('end', function() {
+      // Never reached
+    });
+  });
+
   testReq.on('error', function() {});
   testReq.abort();
 
   return res.status(200).json({
     ok: true,
-    message: 'https.request works'
+    message: 'https.request with callback works'
   });
 };
